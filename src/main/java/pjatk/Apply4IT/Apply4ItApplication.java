52296add7be38e1e20f0dfbb8fad305c89e02875
Hubert Orzelowski <s20467@pjwstk.edu.pl>
2022-09-09 13:11:13 +0200

    Init project

---
+++ src/main/java/pjatk/Apply4IT/Apply4ItApplication.java
@@ -0,0 +1,13 @@
+package pjatk.Apply4IT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apply4ItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apply4ItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pjatk/Apply4IT/Apply4ItApplicationTests.java
@@ -0,0 +1,13 @@
+package pjatk.Apply4IT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apply4ItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
